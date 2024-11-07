bf7d89e33c00f4d3d7c532fdd60e79fdd1f514fd
Su Thet Hnin <suthethnin31@gmail.com>
2024-11-07 23:42:26 +0630

    Final Touch

--- app.js
+++ app.js
@@ -7,18 +7,35 @@ let categoryDisplay = document.querySelector("#category")
 
 let categories = [
     "art",
-    "beauty", "birthday",
-    "cool", "courage",
-    "dating", "death", "dreams",
+    "beauty", 
+    "birthday",
+    "cool", 
+    "courage",
+    "dating", 
+    "death", 
+    "dreams",
     "experience",
-    "failure", "fear", "freedom", "future",
-    "good", "graduation",
-    "happiness", "history", "hope",
-    "imagination", 'inspirational', "intelligence",
-    "knowledge", 'learning', "life", "love",
-    "marriage", "money", "movies",
+    "failure", 
+    "fear", 
+    "freedom", 
+    "future",
+    "good", 
+    "graduation",
+    "happiness", 
+    "history", 
+    "hope",
+    "imagination", 
+    'inspirational', 
+    "intelligence",
+    "knowledge", 
+    'learning', 
+    "life",
+    "love",
+    "marriage", 
+    "money", 
+    "movies",
     "success"
-]
+];
 
 for (let i = 0; i < categories.length; i++) {
     let option = document.createElement('option');
@@ -38,9 +55,12 @@ function generateOutput(event) {
         headers: { 'X-Api-Key': apiKey },
         contentType: 'application/json',
         success: function (result) {
-            categoryDisplay.innerHTML = `${result[0].category}`
+            let category = result[0].category;
+let capitalizedCategory = category.charAt(0).toUpperCase() + category.slice(1);
+
+            categoryDisplay.innerHTML = `${capitalizedCategory}`
             quote.innerHTML = `"${result[0].quote}"`;
-            author.innerHTML = `"${result[0].author}"`;
+            author.innerHTML = `${result[0].author}`;
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
@@ -48,6 +68,37 @@ function generateOutput(event) {
     });
 }
 
+function copyToClipboard() {
+    const quoteText = document.getElementById("quote").innerText;
+    const authorText = document.getElementById("author").innerText;
+    
+    const fullText = `${quoteText} - ${authorText}`;
+    
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      
+      // Fire the toast notification
+      Toast.fire({
+        icon: "success",
+        title: "Signed in successfully"
+      })
+
+    navigator.clipboard.writeText(fullText)
+        .then( Toast.fire({
+      icon: "success",
+      title: "Quote Copies!"
+    }))
+        .catch((error) => console.error("Failed to copy text:", error));
+}
 
 
 
